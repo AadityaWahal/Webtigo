5b7764fe1f16232b60bc526ac85cb3ef7d5d64fc
Aaditya Wahal <somiwahal6287@gmail.com>
2026-03-16 17:02:07 +0530

    Fix Sitemap XML formatting for Google Search Console

--- index.js
+++ index.js
@@ -438,15 +438,13 @@ app.get('/sitemap.xml', (req, res) => {
     const baseUrl = 'https://' + (req.get('host') || 'webtigo.vercel.app');
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages.map(page => `
-            <url>
-                <loc>${baseUrl}${page}</loc>
-                <changefreq>weekly</changefreq>
-                <priority>${page === '/' ? '1.0' : '0.8'}</priority>
-            </url>
-        `).join('')}
-    </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(page => `    <url>
+        <loc>${baseUrl}${page}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>${page === '/' ? '1.0' : '0.8'}</priority>
+    </url>`).join('')}
+</urlset>`;
 
     res.header('Content-Type', 'application/xml');
     res.send(sitemap);
